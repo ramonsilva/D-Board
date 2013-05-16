782d9c10fc41a4bd05fc6bed5b22022a7e0ba805
leandrost <leandro.s.thimoteo@gmail.com>
2013-05-15 23:27:01 -0300

    init app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wiyd::Application.config.secret_token = 'ccea062407a2c9ecc4393d8e8a00f6ba2fcebf29539ae9e8faa98b54df5053d906d39bb3ec762938e8795b880df7e863d8db34b3b158e1b4876ec7d29da4777b'
