35cc24e33e0981359e84f2d9ca7894a39ba214c3
leandrost <leandro.s.thimoteo@gmail.com>
2013-05-18 19:56:50 -0300

    status scopes

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,12 @@ class Task < ActiveRecord::Base
   belongs_to :story
   attr_accessible :description, :status, :story_id
   validates_presence_of :description, :status
+  TO_DO = 1
+  DOING = 2
+  TO_VERIFY = 3
+  DONE = 4
+  scope :to_do, where(status: TO_DO)
+  scope :doing, where(status: DOING)
+  scope :to_verify, where(status: TO_VERIFY)
+  scope :done, where(status: DONE)
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -7,4 +7,17 @@ describe Task do
   it { should allow_mass_assignment_of(:description) }
   it { should allow_mass_assignment_of(:status) }
   it { should allow_mass_assignment_of(:story_id) }
+
+  context "status scope" do
+    before :all do
+      4.times do |x|
+        x += 1
+        Task.create! description: "Task #{x}", status: x 
+      end
+    end
+    it { Task.to_do     == Task.where(status: Task::TO_DO) }
+    it { Task.doing     == Task.where(status: Task::DOING) }
+    it { Task.to_verify == Task.where(status: Task::TO_VERIFY) }
+    it { Task.done      == Task.where(status: Task::DONE) }
+  end
 end
